35dddb1f2d4571a9ee2771f10b8fd7483d79fe86
tldasarath <tldasarath@gmail.com>
2026-04-18 11:53:54 +0530

    feat:Gtag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
+
 import './globals.css';
 import BackgroundEffects from '@/components/BackgroundEffect/BackgroundEffects';
 import { ToastContainer } from 'react-toastify';
@@ -24,6 +26,19 @@ export default function RootLayout({ children }) {
           {' '}
           {/* This wraps Navbar and main content */}
           {children}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-4BP1MP000H"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-4BP1MP000H');
+            `}
+          </Script>
           <ToastContainer
             position="top-right"
             autoClose={3000}
